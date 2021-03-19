374839677865bedbf1a313caeed73eb079fd70b7
Huemac Alfredo Córdova <hmca101292@outlook.com>
2021-03-19 11:57:36 -0600

    Generate username column for the users database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show] 
-  before_action :correct_user, only: [:edit; :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /events or /events.json
   def index
     @events = Event.all
